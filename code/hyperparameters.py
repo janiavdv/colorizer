15b239d0186368bb62ccaed037ab61a336efc4b2
Jania Vandevoorde <jania_vandevoorde@brown.edu>
2024-04-30 13:26:31 -0400

    update hyperparameters

--- code/hyperparameters.py
+++ code/hyperparameters.py
@@ -1,7 +1,6 @@
 """
 Number of epochs. We as you to use only 50 epochs as your budget.
-For extra credit, if you experiment with more complex networks, you
-can change this.
+If you experiment with more complex networks, you can change this.
 """
 num_epochs = 50
 
@@ -34,15 +33,10 @@ Maximum number of weight files to save to checkpoint directory. If
 set to a number <= 0, then all weight files of every epoch will be
 saved. Otherwise, only the weights with highest accuracy will be saved.
 """
-max_num_weights = 5
+max_num_weights = 10
 
 """
 Defines the number of training examples per batch.
-You don't need to modify this.
 """
 batch_size = 10
 
-"""
-The number of image scene classes. Don't change this.
-"""
-num_classes = 15

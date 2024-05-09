6dbf8be64f300c958059e7b7b96d66781d526840
John Farrell <jsfarrell12@gmail.com>
2024-05-09 11:06:58 -0400

    fix gaussian.py

--- code/gaussian.py
+++ code/gaussian.py
@@ -1,5 +1,4 @@
-from tensorflow import expand_dims, range, float32, math, reduce_sum, repeat, reshape, exp, nn
-
+import tensorflow as tf
 """
 Apply gaussian filter to a symbolic tensor.
 
@@ -8,17 +7,17 @@ src: https://stackoverflow.com/a/65219530
 
 def get_gaussian_kernel(shape=(3,3), sigma=0.5):
     m,n = [(ss-1.)/2. for ss in shape]
-    x = expand_dims(range(-n,n+1,dtype=float32),1)
-    y = expand_dims(range(-m,m+1,dtype=float32),0)
-    h = exp(math.divide_no_nan(-((x*x) + (y*y)), 2*sigma*sigma))
-    h = math.divide_no_nan(h,reduce_sum(h))
+    x = tf.expand_dims(tf.range(-n,n+1,dtype=tf.float32),1)
+    y = tf.expand_dims(tf.range(-m,m+1,dtype=tf.float32),0)
+    h = tf.exp(tf.math.divide_no_nan(-((x*x) + (y*y)), 2*sigma*sigma))
+    h = tf.math.divide_no_nan(h,tf.reduce_sum(h))
     return h
 
 def gaussian_blur(inp, shape=(3,3), sigma=0.5):
-    in_channel = shape(inp)[-1]
+    in_channel = tf.shape(inp)[-1]
     k = get_gaussian_kernel(shape,sigma)
-    k = expand_dims(k,axis=-1)
-    k = repeat(k,in_channel,axis=-1)
-    k = reshape(k, (*shape, in_channel, 1))
-    conv = nn.depthwise_conv2d(inp, k, strides=[1,1,1,1],padding="SAME")
+    k = tf.expand_dims(k,axis=-1)
+    k = tf.repeat(k,in_channel,axis=-1)
+    k = tf.reshape(k, (*shape, in_channel, 1))
+    conv = tf.nn.depthwise_conv2d(inp, k, strides=[1,1,1,1],padding="SAME")
     return conv

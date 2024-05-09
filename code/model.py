0858e66e5151a5e844c02f27264800d4908d5065
John Farrell <jsfarrell12@gmail.com>
2024-05-09 10:20:55 -0400

    leaky relu

--- code/model.py
+++ code/model.py
@@ -2,6 +2,7 @@ import keras
 from keras.layers import \
      concatenate, Conv2DTranspose, Rescaling, BatchNormalization
 from keras.applications import VGG19
+from keras.layers import LeakyReLU
 import hyperparameters as hp
 import numpy as np
 from skimage.filters import gaussian
@@ -82,7 +83,8 @@ class Model():
         =================================================================
         """
         self.mod = vgg19.output
-        b = Conv2DTranspose(filters=512, kernel_size=3, strides=2, activation="relu", padding="same")(self.mod)
+        b = Conv2DTranspose(filters=512, kernel_size=3, strides=2, activation=None, padding="same")(self.mod)
+        b = LeakyReLU(alpha=0.1)(b)
         b = BatchNormalization()(b)
         self.mod = concatenate([b, vgg19.get_layer("block5_conv4").output])
 
@@ -94,14 +96,17 @@ class Model():
         ]
 
         for filters, layer_name in block_layer_sizes:
-            b = Conv2DTranspose(filters=filters, kernel_size=3, strides=1, activation="relu", padding="same")(self.mod)
+            b = Conv2DTranspose(filters=filters, kernel_size=3, strides=1, activation=None, padding="same")(self.mod)
+            b = LeakyReLU(alpha=0.1)(b)
             b = BatchNormalization()(b)
-            b = Conv2DTranspose(filters=filters, kernel_size=3, strides=2, activation="relu", padding="same")(b)
+            b = Conv2DTranspose(filters=filters, kernel_size=3, strides=2, activation=None, padding="same")(b)
+            b = LeakyReLU(alpha=0.1)(b)
             b = BatchNormalization()(b)
            
             self.mod = concatenate([b, vgg19.get_layer(layer_name).output])
 
-        self.mod = Conv2DTranspose(64, 3, activation="relu", padding="same")(self.mod)
+        self.mod = Conv2DTranspose(64, 3, activation=None, padding="same")(self.mod)
+        self.mod = LeakyReLU(alpha=0.1)(self.mod)
         self.mod = BatchNormalization()(self.mod)
         self.mod = Conv2DTranspose(2, 3, activation="sigmoid", padding="same")(self.mod)
         self.mod = Rescaling(scale=255.0, offset=-128)(self.mod)
